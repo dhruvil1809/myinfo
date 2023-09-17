9decdc58cec9c8be11414e1843befa7fdb6a1d53
dhruvil1809 <dhruvilantal123a@gmail.com>
2023-09-17 10:33:24 +0530

    add

--- info/settings.py
+++ info/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
